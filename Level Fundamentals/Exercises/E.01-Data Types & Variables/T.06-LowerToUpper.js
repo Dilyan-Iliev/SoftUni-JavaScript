53a440159e1b30dfa3e6e386f57872259dc599f1
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-09-12 11:00:01 +0200

    Add tasks from first exercise JS-Funds

---
+++ Level Fundamentals/Exercises/E.01-Data Types & Variables/T.06-LowerToUpper.js
@@ -0,0 +1,10 @@
+function lowerToUpper(letter){
+    
+    if (letter === letter.toUpperCase()){
+        console.log('upper-case');
+    }
+    else {
+        console.log('lower-case');
+    }
+}
+lowerToUpper("L")
