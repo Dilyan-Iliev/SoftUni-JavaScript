0b6e7643972e781c6713270486ce586c518734da
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-09-12 10:58:48 +0200

    Add tasks from first exercise JS-Funds

---
+++ Level Fundamentals/Exercises/E.01-Data Types & Variables/T.02-CharToString.js
@@ -0,0 +1,9 @@
+function charToString(firstChar, secondChar, thirdChar) {
+    let concat = firstChar + secondChar + thirdChar;
+
+    console.log(concat);
+}
+charToString('a',
+    'b',
+    'c'
+)
