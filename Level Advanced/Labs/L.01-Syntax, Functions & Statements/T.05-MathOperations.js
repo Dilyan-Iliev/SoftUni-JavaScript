8bc2e553ac207d19c090e91e9962db56a365dbc3
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-09-20 16:20:59 +0200

    Add tasks from first lection JS-Advanced

---
+++ Level Advanced/Labs/L.01-Syntax, Functions & Statements/T.05-MathOperations.js
@@ -0,0 +1,15 @@
+function solve(num1, num2, operation) {
+    let result;
+
+    switch (operation) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+        case '%': result = num1 % num2; break;
+        case '**': result = num1 ** num2; break;
+    }
+    return result;
+}
+
+console.log(solve(5, 6, '+'));
