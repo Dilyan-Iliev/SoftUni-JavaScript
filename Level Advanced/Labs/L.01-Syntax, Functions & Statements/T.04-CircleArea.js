1f095886c915208771a59da39af10ddb6d63a25a
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-09-20 16:20:39 +0200

    Add tasks from first lection JS-Advanced

---
+++ Level Advanced/Labs/L.01-Syntax, Functions & Statements/T.04-CircleArea.js
@@ -0,0 +1,10 @@
+function solve(input) {
+    if (typeof (input) !== 'number') {
+        return `We can not calculate the circle area, because we receive a ${typeof (input)}.`
+    }
+
+    return (Math.pow(input, 2) * Math.PI).toFixed(2);
+}
+
+console.log(solve(5));
+console.log(solve('name'));
