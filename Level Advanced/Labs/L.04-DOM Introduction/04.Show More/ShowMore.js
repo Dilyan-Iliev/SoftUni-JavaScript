0c58922e498a930ecba1b7746ac513395c7374a6
Dilyan-Iliev <dilqnn.ilievv@abv.bg>
2022-10-05 18:02:34 +0200

    Add tasks from fourth lection JS-Advanced

---
+++ Level Advanced/Labs/L.04-DOM Introduction/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let hiddenText = document.getElementById('text');
+    hiddenText.style.display = 'inline';
+
+    let textToHide = document.getElementById('more');
+    textToHide.style.display = 'none';
+}
